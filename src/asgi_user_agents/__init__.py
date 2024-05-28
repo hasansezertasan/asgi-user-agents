1c8f37d0e9cf714a5a7805a291572f413274a3c1
hasansezertasan <hasansezertasan@gmail.com>
2024-05-29 00:59:14 +0300

    Remove duplicate `__version__` tag alongside with `__all__` tag from `__init__.py` and implement relative imports.

--- src/asgi_user_agents/__init__.py
+++ src/asgi_user_agents/__init__.py
@@ -1,16 +1,7 @@
 from contextlib import suppress
 
-from asgi_user_agents.datastructures import UADetails as UADetails
-from asgi_user_agents.middleware import UAMiddleware as UAMiddleware
+from .datastructures import UADetails as UADetails
+from .middleware import UAMiddleware as UAMiddleware
 
 with suppress(ImportError):
-    from asgi_user_agents.requests import UARequest as UARequest
-
-__version__ = "0.1.0"
-
-__all__ = [
-    "__version__",
-    "UAMiddleware",
-    "UADetails",
-    "UARequest",
-]
+    from .requests import UARequest as UARequest
